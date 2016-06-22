b0bcbc94f712ae6f88e1d5448a3588ff61522d3c
Ben Spencer <ben.spencer@healthforge.io>
2016-06-22 15:30:51 +0100

    Flesh out test script.

--- test/start.sh
+++ test/start.sh
@@ -1,3 +1,10 @@
-#!/bin/sh
+#!/bin/sh -e
 
-docker run -d --name sftp --env-file=sftp/env -v $(pwd)/sftp/ssh:/etc/ssh:ro sftp
+docker rm -f sftp || true
+docker build -t sftp ../main
+
+docker rm -f sftptest || true
+docker build -t sftptest .
+
+docker run -d --name sftptest -p 1389:389 -p 8080:8080 sftptest
+docker run -d --name sftp --link sftptest:sftptest -p 2222:22 --env-file=sftp/env -v $(pwd)/sftp/ssh:/etc/ssh:ro sftp
