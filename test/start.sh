8513f2ceaeea6e8db1494514dfafd975f6b1899a
Ben Spencer <ben.spencer@healthforge.io>
2016-06-24 12:57:16 +0100

    Create test realm and user.

--- test/start.sh
+++ test/start.sh
@@ -1,10 +1,45 @@
 #!/bin/sh -e
 
+KEYCLOAK_PORT=8080
+KEYCLOAK_BASE="http://localhost:$KEYCLOAK_PORT/auth"
+
 docker rm -f sftp || true
 docker build -t sftp ../main
 
 docker rm -f sftptest || true
 docker build -t sftptest .
 
-docker run -d --name sftptest -p 1389:389 -p 8080:8080 sftptest
+docker run -d --name sftptest -p 1389:389 -p $KEYCLOAK_PORT:8080 sftptest
 docker run -d --name sftp --link sftptest:sftptest -p 2222:22 --env-file=sftp/env -v $(pwd)/sftp/ssh:/etc/ssh:ro sftp
+
+echo
+
+echo -n "Waiting for keycloak to start..."
+while ! curl -s -I -f $KEYCLOAK_BASE >/dev/null; do
+    echo -n .
+    sleep 1
+done
+echo " ok"
+
+echo -n "Requesting token... "
+TKN=$(curl -s "$KEYCLOAK_BASE/realms/master/protocol/openid-connect/token" \
+ -H "Content-Type: application/x-www-form-urlencoded" \
+ -d "username=admin" \
+ -d 'password=secret' \
+ -d 'grant_type=password' \
+ -d 'client_id=admin-cli' | jq -r '.access_token')
+echo " ok"
+
+echo -n "Creating test realm... "
+curl -s "$KEYCLOAK_BASE/admin/realms" \
+ -H "Content-Type: application/json" \
+ -H "Authorization: Bearer $TKN" \
+ --data-binary @keycloak/realm.json
+echo " ok"
+
+echo -n "Creating test user... "
+curl  "$KEYCLOAK_BASE/admin/realms/test/users" \
+ -H "Content-Type: application/json" \
+ -H "Authorization: Bearer $TKN" \
+ --data-binary @keycloak/user.json
+echo " ok"
